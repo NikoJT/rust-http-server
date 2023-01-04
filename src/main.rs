d340116181e010c639c852c0d7e4d2d89cea6ead
NikoJT <niko.tarmaa@gmail.com>
2023-01-04 20:11:18 +0200

    added websitehandler struct and handler trait

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
 // include server module and http module.
 mod server;
 mod http;
+mod website_handler;
 
 // include Server struct from server
 use server::Server;
-
+use website_handler::WebsiteHandler;
 fn main() {
     // Call and create a new instance
     let server = Server::new("127.0.0.1:8080".to_string());
     // Call and run on Server.
-    server.run();
+    server.run(WebsiteHandler);
 }
